577b2c41df45b2f6f19b26abafa4052f1309b7b6
Eduardo Hilário <48762158+eduardohilariodev@users.noreply.github.com>
2024-01-22 00:27:21 -0300

    chore: add react files

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        oiuwer
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
